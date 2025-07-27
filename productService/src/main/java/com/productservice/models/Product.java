4d77f28cc37826babcc2efef8fedae5b3f968146
rahul-teli <rahulteli3018@gmail.com>
2025-07-27 15:48:40 +0530

    Repository Layer

--- productService/src/main/java/com/productservice/models/Product.java
+++ productService/src/main/java/com/productservice/models/Product.java
@@ -1,15 +1,21 @@
 package com.productservice.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @Getter
 @Setter
+@Entity
 public class Product extends BaseModel {
 
     private String title;
     private String description;
     private double price;
     private String imageUrl;
+    @ManyToOne
     private Category category;
 }
---
+++ productService/src/main/java/com/productservice/repositories/ProductRepository.java
@@ -0,0 +1,7 @@
+package com.productservice.repositories;
+
+import com.productservice.models.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
